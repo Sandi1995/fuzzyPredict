f4e738677a1ac30f8a48b7acf3fde22eb910f54a
Sandi1995 <paul.sandipan04@gmail.com>
2019-04-29 17:11:09 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 28 21:30:04 2019
-
-@author: dexter
-"""
-
 import pandas as pd
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
@@ -15,17 +7,19 @@ def match_name(item,item_list,min_score=0):
     # Returning empty name for no match as well
     max_name = ""
     # Iternating over all names in the other
-    for name2 in list_names:
+    for i in item_list:
         #Finding fuzzy match score
-        score = fuzz.ratio(name, name2)
+        score = fuzz.ratio(item, i)
         # Checking if we are above our threshold and have a better score
         if (score > min_score) & (score > max_score):
-            max_name = name2
+            max_name = i
             max_score = score
     return (max_name, max_score)
 
 data=pd.read_csv("test_cvs.csv")
-data1=data.dropna()
-for name in data1[['address','phone']].values.tolist():
-    match=match_name()
-
+master=data.dropna()
+missing_name=data[data.names.isnull()]
+for item in missing_name[['phone','address']].values.tolist():                                                                                  
+     match=match_name(item,master[['phone','address']].values.tolist(),75)                                                                       
+     print("name :",master.loc[master['phone']==match[0][0],'names'].tolist()[0],'score: ',match[1]) 
+ 
